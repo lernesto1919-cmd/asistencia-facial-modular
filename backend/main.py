83b650f2c2637f5bf1854fe44dbb96daf83734fd
lernesto1919-cmd <l.ernesto1919@gmail.com>
2026-05-03 17:32:58 -0600

    Agregar endpoint registrar

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# Modelo de datos
+class Asistencia(BaseModel):
+    alumno: str
+
 
 @app.get("/")
 def inicio():
     return {
         "mensaje": "Sistema de asistencia facial funcionando"
+    }
+
+
+@app.post("/registrar")
+def registrar_asistencia(data: Asistencia):
+
+    print(
+        f"Asistencia recibida: {data.alumno}"
+    )
+
+    return {
+        "status": "ok",
+        "alumno": data.alumno
     }
